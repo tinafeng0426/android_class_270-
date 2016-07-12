f25ff8c5a47a15054fd82849bd8eba5f15c8e2df
tinafeng0426 <mush0426>
2016-07-12 12:05:47 +0800

    待修

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -5,17 +5,49 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                return false;
+            }
+        });
     }
+    public void submit(View view)
+    {
+        String text= editText.getText().toString();
+        
+        text = text + "性別" + SelectedSex;
 
+        textView.setText(text);
+        editText.setText("");
+        RadioGroup radioGroup;
+
+        String SelectedSex = "男";
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        radioGroup.setOnKeyListener(new RadioGroup.onKeyListener);
+
+
+    }
 }
